020ab58dc9336d2cb5a037121a742a3da67fa5e9
guojing <guojing64745@offcn.com>
2019-12-17 11:52:15 +0800

    处理请求body数据 和 处理请求header

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -2,6 +2,11 @@ const toString = Object.prototype.toString
 export function isDate (val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
-export function isObject(val: any): val is Object {
-  return val !== null && typeof val === 'object'
+// 对于FormData 、ArrayBuffer也判断为true 这些数据类型send()的时候是不用处理的
+// export function isObject(val: any): val is Object {
+//   return val !== null && typeof val === 'object'
+// }
+// 判断是否是普通对象
+export function isPlainObject (val: any): val is Object {
+  return toString.call(val) === '[object Object]'
 }
